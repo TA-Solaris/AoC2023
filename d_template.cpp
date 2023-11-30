d4f7a8192656b1fb95de1794a8430a15a1b8000e
Edward Potter <74555065+TA-Solaris@users.noreply.github.com>
2023-11-30 20:45:50 +0000

    Created template

---
+++ d_template.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+using namespace std;
+
+int part1(string input) {
+    return 0;
+}
+
+int part2(string input) {
+    return 0;
+}
+
+string file_to_string(string filename) {
+    ifstream inFile;
+    inFile.open(filename);
+    if (inFile.fail()) {
+        cerr << "Unable to open file " << filename << " for reading!" << endl;
+        exit(1);
+    }
+    ostringstream ss;
+    ss << inFile.rdbuf();
+    return ss.str();
+}
+
+int main() {
+    string input = file_to_string("input.txt");
+    // Running AoC
+    cout << "--- Part 1 ---" << endl;
+    cout << part1(input);
+    cout << endl << "--- Part 2 ---" << endl;
+    cout << part2(input) << endl;
+    exit(0);
+}
