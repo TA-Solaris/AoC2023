dea5d80d40e80b6059cfba35211811a9b4dff3f2
Edward Potter <74555065+TA-Solaris@users.noreply.github.com>
2023-12-22 23:38:46 +0000

    Day 9 Part 1 Completed

---
+++ d9/d9.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <regex>
+using namespace std;
+
+vector<int> getNumbers(string input) {
+    vector<int> ns;
+    stringstream ss(input);
+    string n;
+    while (getline(ss, n, ' ')) {
+        ns.emplace_back(stoi(n));
+    }
+    return ns;
+}
+
+int getNext(vector<int> seq) {
+    if(all_of(seq.cbegin(), seq.cend(), [](int i){ return i == 0; })) return 0;
+    vector<int> diffs;
+    int current = seq[0];
+    for (int i = 1; i < seq.size(); i++) {
+        diffs.emplace_back(seq[i] - current);
+        current = seq[i];
+    }
+    return seq[seq.size() - 1] + getNext(diffs);
+}
+
+int part1(string input) {
+    int total = 0;
+
+    stringstream ss(input);
+    string line;
+    while (getline(ss, line, '\n')) {
+        total += getNext(getNumbers(line));
+    }
+
+    return total;
+}
+
+int part2(string input) {
+    return 0;
+}
+
+string file_to_string(string filename) {
+    ifstream inFile;
+    inFile.open(filename);
+    if (inFile.fail()) {
+        cerr << "Unable to open file " << filename << " for reading!" << endl;
+        exit(1);
+    }
+    ostringstream ss;
+    ss << inFile.rdbuf();
+    return ss.str();
+}
+
+int main() {
+    string input = file_to_string("input.txt");
+    // Running AoC
+    cout << "--- Part 1 ---" << endl;
+    cout << part1(input);
+    cout << endl << "--- Part 2 ---" << endl;
+    cout << part2(input) << endl;
+    exit(0);
+}
